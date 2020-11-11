72334f587e4de688e1ca876a73fadb8ecea17b56
Oleg Golubev <git@golubevog.ru>
2020-11-12 01:17:48 +0300

    add axes

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ console.log('start');
 
 const canvas = document.getElementById("canvasid");
 let ctx = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
 
 let drawCoordPlane = function(step){
-    const width = canvas.width;
-    const height = canvas.height;
-    
+        
     for (let x=0;x<width;x += step){
         ctx.moveTo(x, 0);
         ctx.lineTo(x,height);
@@ -14,13 +14,27 @@ let drawCoordPlane = function(step){
     for (let y=0;y<height;y += step){
         ctx.moveTo(0, y);
         ctx.lineTo(width, y);
+    ctx.lineWidth = 1;
     ctx.stroke();
     }
+    ctx.closePath();
+}
+
+let drawMainAxes = function(){
+    ctx.beginPath();
+    ctx.moveTo(0, height/2);
+    ctx.lineTo(width,height/2);
+    ctx.moveTo(width/2, 0);
+    ctx.lineTo(width/2, height);
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.closePath();
 }
 
 
 const step = 10;
 let a = drawCoordPlane(step);
+let b = drawMainAxes();
 ctx.stroke();
 console.log(a,'hello');
 
