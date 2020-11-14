ee1ed2a91a37827956a628a0ed9a85f50159d73d
Oleg Golubev <git@golubevog.ru>
2020-11-14 08:16:52 +0300

    add button

--- script.js
+++ script.js
@@ -4,23 +4,47 @@ const canvas = document.getElementById("canvasid");
 let ctx = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
+const lineStepW = width / 10;
+
+console.log(lineStepW,"lineStepW");
+
 
 let drawCoordPlane = function(step){
-        
+    ctx.strokeStyle = "grey";
+
+    let count = 0;
     for (let x=0;x<width;x += step){
+        ctx.beginPath();    
+        if (count % 5 == 0) {
+            ctx.lineWidth = 3;
+            
+        }else{
+            ctx.lineWidth = 1;
+        }
         ctx.moveTo(x, 0);
         ctx.lineTo(x,height);
+        ctx.stroke();
+        count += 1;
+        ctx.closePath();
     }
     for (let y=0;y<height;y += step){
+        ctx.beginPath();    
+        if (count % 5 == 0) {
+            ctx.lineWidth = 3;
+        }else{
+            ctx.lineWidth = 1;
+        }
         ctx.moveTo(0, y);
         ctx.lineTo(width, y);
-    ctx.lineWidth = 1;
-    ctx.stroke();
+        ctx.stroke();
+        count += 1;
+        ctx.closePath()
     }
     ctx.closePath();
 }
 
 let drawMainAxes = function(){
+    ctx.strokeStyle = "black";
     ctx.beginPath();
     ctx.moveTo(0, height/2);
     ctx.lineTo(width,height/2);
@@ -32,22 +56,32 @@ let drawMainAxes = function(){
 }
 
 let drawTextAxes = function (){
-    ctx.font = "8px Arial";
-    ctx.fillStyle = 'white';
-    ctx.fillText('Hello world', 20, 50);
+    ctx.font = "7px Arial";
+    ctx.fillStyle = 'black';
     for (let x =0; x<width; x+= step){
-        ctx.fillText(x - 400, x, height/2 + 10);
+        ctx.fillText(x - 400 + 5, x, height/2 + 10);
     }
 
     for (let y =0; y<height; y+= step){
-        ctx.fillText(y - height/2, width/2 - 15, y);
+        ctx.fillText(y - height/2, width/2 - 19, y);
+    }
+}
+
+let graphQuad = function(){
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    for (let x =0; x<width; x+=>){
+        let y = x*x;
+        ctx.lineTo(x,y);
+    cst.stroke();
     }
 }
 
 const step = 20;
 drawCoordPlane(step);
 drawMainAxes();
-drawTextAxes();
+graphQuad();
+//drawTextAxes();
 
 
 console.log(a,'hello');
