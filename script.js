5db9ba0c11fa3207f5e271ee2d99ba1e6ad61076
Oleg Golubev <git@golubevog.ru>
2020-11-18 00:05:22 +0300

    добавлены кнопки рисования графико и очистки поля

--- script.js
+++ script.js
@@ -1,92 +1,93 @@
-console.log('start');
+console.log("start");
 
 const canvas = document.getElementById("canvasid");
-let ctx = canvas.getContext('2d');
+let ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
 const lineStepW = width / 10;
+const moveToZeroY = height / 2;
+const moveToZeroX = width / 2;
 
-console.log(lineStepW,"lineStepW");
+console.log(lineStepW, "lineStepW");
 
-
-let drawCoordPlane = function(step){
+let drawCoordPlane = function(step) {
     ctx.strokeStyle = "grey";
-
     let count = 0;
-    for (let x=0;x<width;x += step){
-        ctx.beginPath();    
+    for (let x = 0; x < width; x += step) {
+        ctx.beginPath();
         if (count % 5 == 0) {
             ctx.lineWidth = 3;
-            
-        }else{
+        } else {
             ctx.lineWidth = 1;
         }
         ctx.moveTo(x, 0);
-        ctx.lineTo(x,height);
+        ctx.lineTo(x, height);
         ctx.stroke();
         count += 1;
         ctx.closePath();
     }
-    for (let y=0;y<height;y += step){
-        ctx.beginPath();    
+    for (let y = 0; y < height; y += step) {
+        ctx.beginPath();
         if (count % 5 == 0) {
             ctx.lineWidth = 3;
-        }else{
+        } else {
             ctx.lineWidth = 1;
         }
         ctx.moveTo(0, y);
         ctx.lineTo(width, y);
         ctx.stroke();
         count += 1;
-        ctx.closePath()
+        ctx.closePath();
     }
     ctx.closePath();
-}
 
-let drawMainAxes = function(){
+    drawMainAxes();
+};
+
+let drawMainAxes = function() {
     ctx.strokeStyle = "black";
     ctx.beginPath();
-    ctx.moveTo(0, height/2);
-    ctx.lineTo(width,height/2);
-    ctx.moveTo(width/2, 0);
-    ctx.lineTo(width/2, height);
+    ctx.moveTo(0, height / 2);
+    ctx.lineTo(width, height / 2);
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2, height);
     ctx.lineWidth = 5;
     ctx.stroke();
     ctx.closePath();
-}
+};
 
-let drawTextAxes = function (){
+let drawTextAxes = function() {
     ctx.font = "7px Arial";
-    ctx.fillStyle = 'black';
-    for (let x =0; x<width; x+= step){
-        ctx.fillText(x - 400 + 5, x, height/2 + 10);
+    ctx.fillStyle = "black";
+    for (let x = 0; x < width; x += step) {
+        ctx.fillText(x - 400 + 5, x, height / 2 + 10);
     }
 
-    for (let y =0; y<height; y+= step){
-        ctx.fillText(y - height/2, width/2 - 19, y);
+    for (let y = 0; y < height; y += step) {
+        ctx.fillText(y - height / 2, width / 2 - 19, y);
     }
-}
+};
 
-let graphQuad = function(){
+let drawFunctionQuad = function(power) {
+    console.log("рисование графика");
+    const delta_x = step / 4;
     ctx.beginPath();
-    ctx.moveTo(0,0);
-    for (let x =0; x<width; x+=>){
-        let y = x*x;
-        ctx.lineTo(x,y);
-    cst.stroke();
+    ctx.moveTo(moveToZeroX, moveToZeroY);
+    for (let x = -width; x < width; x += delta_x) {
+        let y = Math.pow(x / step, power);
+        ctx.lineTo(x + width / 2, -1 * y + height / 2);
     }
-}
+    ctx.stroke();
+};
 
-const step = 20;
+let clearCanvas = function() {
+    console.log("очистка экрана");
+    ctx.clearRect(0, 0, width, height);
+    drawCoordPlane(step);
+};
+
+//рисование координатной плоскости в основной программе
+const step = 50;
 drawCoordPlane(step);
-drawMainAxes();
-graphQuad();
-//drawTextAxes();
 
-
-console.log(a,'hello');
-
-
-
-
-console.log('stop');
+console.log("stop");
